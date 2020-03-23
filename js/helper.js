19448f349e1f23f0b3701c0226d2c8347717427c
n-pola <n.polarek@live.de>
2020-03-23 17:39:22 +0100

    sticky menu

--- js/helper.js
+++ js/helper.js
@@ -7,3 +7,21 @@ christian.noss@th-koeln.de
 https://twitter.com/cnoss
 https://th-koeln.github.io/mi-bachelor-screendesign/
 ----------------------------------------------------------------------------*/
+jQuery(document).ready(function($) {
+	$(document).on("scroll", function() {
+		let scrollWindow = $(document).scrollTop();
+		let wHeight = $(window).height();
+		if (scrollWindow >= wHeight) {
+			$("#sticky-nav").addClass("sticky-nav-active");
+			$("#l-mainContent").css(
+				"margin-top",
+				$("#sticky-nav").innerHeight() + 48 + "px"
+			);
+		} else {
+			$("#sticky-nav").removeClass("sticky-nav-active");
+			$("#l-mainContent").css("margin-top", "0px");
+		}
+	});
+});
+
+console.log("test");
